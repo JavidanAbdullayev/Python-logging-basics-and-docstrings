7ba5cb254c44c9c20f80e698f1d8f51a25105212
JavidanAbdullayev <cavidan5889@gmail.com>
2023-02-16 21:29:21 +0100

    logging added for all models

--- or.py
+++ or.py
@@ -1,11 +1,24 @@
 import pandas as pd
 from utils.all_utils import prepare_data, save_plot
 from utils.model import Perceptron
+import logging
+import os
 
+gate = 'OR gate'
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join('logs', 'running_logs.log'),
+    level=logging.INFO,
+    format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
+    filemode='a',
+)
 
 def main(data, modelName, plotName, eta, epochs):
 
     df = pd.DataFrame(data)
+    logging.info(f'This is the raw dataset: {df}')
     X, y = prepare_data(df)
 
     model = Perceptron(eta=eta, epochs=epochs)
@@ -27,8 +40,12 @@ if __name__ == '__main__':
 
     ETA = 0.1 # BTW 0 AND 1
     EPOCHS = 10
-
-    main(data=OR, modelName='or.model', plotName='or.png', eta=ETA, epochs=EPOCHS)
-
+    try:
+        logging.info(f'>>>>> starting training for {gate}>>>>>')
+        main(data=OR, modelName='or.model', plotName='or.png', eta=ETA, epochs=EPOCHS)
+        logging.info(f'<<<<<< done training for {gate} <<<<<<')
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
